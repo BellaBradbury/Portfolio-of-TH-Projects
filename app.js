8a3edee4b0300ae21140e6a47b09b266eb426fa8
BellaBradbury <bellabradbury@yahoo.com>
2022-10-19 16:06:19 -0400

    about page updated

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// REQUIRES DEPENDENCIES
 const express = require('express');
 const data = require('./data.json');
 const app = express();
 
+// MIDDLEWARE
 app.set('view engine', 'pug');
 app.use( '/static', express.static('public') );
 
+// ROUTES
 app.get( '/', (req, res) => {
   res.locals.projects = data.projects;
   res.render('index');
@@ -17,10 +20,24 @@ app.get( '/projects/:id', (req, res) => {
   res.render( 'project', {projects: data.project[index]} );
 });
 
+// ERROR HANDLING
+app.use( (req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = "Whoops! The page you're looking for doesn't exist.";
+  console.log(err.status, err.message);
+  next(err);
+});
+app.use( (err, req, res, next) => {
+  if (err.status !== 404) {
+    err.status = 500;
+    err.message = "Something has gone wrong, please try again!";
+    console.log(err.status, err.message);
+  }
+});
 
 
-
-
+// STARTS SERVER
 app.listen(3000, () => {
   console.log('This app is currently running on port 3000');
 });
